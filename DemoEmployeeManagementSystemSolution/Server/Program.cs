f71c54fc3a965d9a3365ae870f2c719cc0e9dd82
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2025-07-13 22:32:32 +0500

    finnish swagger bugs

--- DemoEmployeeManagementSystemSolution/Server/Program.cs
+++ DemoEmployeeManagementSystemSolution/Server/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using Scalar.AspNetCore;
 using ServerLibrary.Data;
 using ServerLibrary.Helpers;
 using ServerLibrary.Repositories.Contracts;
